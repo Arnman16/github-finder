d01d7365acf6fe558255acf09780291d16fa7e2c
Aaron Noseworthy <aaron.noseworthy@gmail.com>
2021-11-29 13:39:24 -0330

    added NotFound page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Alert from "./components/layout/Alert";
-import Users from "./components/users/Users";
 import User from "./components/users/User";
-import Search from "./components/users/Search";
 import About from "./components/pages/About";
 import GithubState from "./context/github/GithubState";
 import AlertState from "./context/alert/AlertState";
+import Home from "./components/pages/Home";
+import NotFound from "./components/pages/NotFound";
 import "./App.css";
 
 const App = () => {
@@ -20,18 +20,11 @@ const App = () => {
             <div className="container">
               <Alert />
               <Routes>
-                <Route
-                  exact
-                  path="/"
-                  element={
-                    <Fragment>
-                      <Search />
-                      <Users />
-                    </Fragment>
-                  }
-                />
+                <Route exact path="/" element={<Home />} />
                 <Route exact path="/about" element={<About />} />
                 <Route exact path="/user/:login" element={<User />} />
+                <Route path="/*" element={<NotFound />} />
+                {/* <Route element={<NotFound />} /> */}
               </Routes>
             </div>
           </div>
---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+
+const NotFound = () => {
+  return (
+    <Fragment>
+      <div className="container fill-height">
+        <h1 className="all-center">Not Found</h1>
+        <p className="lead all-center">The page you are looking for does not exist...</p>
+      </div>
+    </Fragment>
+  );
+};
+
+export default NotFound;
